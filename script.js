618e16ab2ebca8b46cbf87b1cc87cce08768b2df
Maximiliano Bernard <bmaximiliano0210@gmail.com>
2024-03-14 02:32:30 -0300

    Add random RGB hover

--- script.js
+++ script.js
@@ -7,8 +7,17 @@ function makeGrid(rows, cols) {
       let cell = document.createElement("div");
       cell.className = "cell";
       cell.addEventListener("mouseenter", (e) => {
+        if (document.querySelector(".random-state").textContent === "ON") {
+          let r = Math.floor(Math.random() * 255) + 1;
+          let g = Math.floor(Math.random() * 255) + 1;
+          let b = Math.floor(Math.random() * 255) + 1;
+          brushColor = `rgb(${r},${g},${b})`;
+          colorPicker.value = brushColor;
+        } else {
+          brushColor = colorPicker.value;
+        }
         if (e.ctrlKey) {
-          cell.style.backgroundColor = brushColor;
+          cell.style.backgroundColor = "white";
         } else cell.style.backgroundColor = brushColor;
       });
       row.appendChild(cell);
@@ -21,6 +30,16 @@ let brushColor = "black";
 let colorPicker = document.querySelector("#color-picker");
 let gridContainer = document.querySelector("#grid-container");
 let gridSizeInput = document.querySelector("#grid-size");
+let randomRGB = document.querySelector("#random-rgb");
+
+randomRGB.addEventListener("click", (e) => {
+  let state = document.querySelector(".random-state");
+  if (state.textContent === "OFF") {
+    state.textContent = "ON";
+  } else {
+    state.textContent = "OFF";
+  }
+});
 colorPicker.addEventListener("input", (e) => {
   brushColor = e.target.value;
 });

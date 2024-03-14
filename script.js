dc6ce0b10188eda4792d119d0559b52740bdf148
Maximiliano Bernard <bmaximiliano0210@gmail.com>
2024-03-14 01:09:03 -0300

    Add Grid Maker

---
+++ script.js
@@ -0,0 +1,14 @@
+function makeGrid(rows, cols) {
+  let gridContainer = document.querySelector("#grid-container");
+  for (let i = 0; i < rows; i++) {
+    let row = document.createElement("div");
+    row.className = "row";
+    for (let i = 0; i < cols; i++) {
+      let cell = document.createElement("div");
+      cell.className = "cell";
+      row.appendChild(cell);
+    }
+    gridContainer.appendChild(row);
+  }
+}
+makeGrid(3, 3);
